19a81171195bf9dc66310effc8b185bb5a90743d
Vinchd <vince.daviaud@gmail.com>
2025-09-15 18:44:40 +0200

    create informations page

---
+++ src/app/informations/page.jsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <main className="flex justify-center items-center bg-[#0D0200] w-full h-full text-[#E3D7C9] text-4xl">
+      Work in Progress
+    </main>
+  );
+}
