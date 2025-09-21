2891b42faa9e5cfae4f4724e96a6fe02e6997430
Vinchd <vince.daviaud@gmail.com>
2025-09-22 00:13:54 +0200

    add jsonLD on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,31 @@
 import Image from "next/image";
 
 export default function Home() {
+  const jsonLD = {
+    "@context": "https://schema.org",
+    "@type": "Restaurant",
+    name: "La Boule rouge",
+    image: "https://www.laboulerougeparis.fr/logo-opengraph.png",
+    "@id": "https://www.laboulerougeparis.fr/",
+    url: "https://www.laboulerougeparis.fr/",
+    // telephone: "+33 2 40 82 91 71",
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "1 rue de la Boule Rouge",
+      addressLocality: "Pornic",
+      postalCode: "75009",
+      addressCountry: "FR",
+    },
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: 48.87327,
+      longitude: 2.34459,
+    },
+    servesCuisine: ["Française"],
+    priceRange: "€€",
+    openingHours: ["We-Su 19:00-02:00"],
+  };
+
   return (
     <main className="relative bg-primary h-full overflow-hidden">
       <Image
