47924fa48ef55548949edd373346ff0a881eeff1
Vinchd <vince.daviaud@gmail.com>
2025-09-24 16:50:24 +0200

    add phone tel on informations page

--- src/app/informations/page.jsx
+++ src/app/informations/page.jsx
@@ -2,20 +2,24 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function page() {
   return (
-    <main className="flex flex-col justify-center items-center w-full h-full text-center">
-      <h1 className="mb-16 text-4xl">Horaires d'ouverture</h1>
+    <main className="flex flex-col justify-center items-center py-12 w-full min-h-full overflow-y-auto text-center scrollbar-hide">
+      <h1 className="mb-12 text-4xl">Horaires d'ouverture</h1>
       <p className="font-bickhamscript text-4xl">Mercredi au Dimanche 19h-2h</p>
+      <a
+        href="tel:+33670814574"
+        className="mt-8 font-bickhamscript text-4xl hover:scale-105 duration-300 ease-in"
+      >
+        06 70 81 45 74
+      </a>
       <a
         href="https://maps.app.goo.gl/MA4Dr2Psv33UdBrP6"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="Instagram"
-        className="mt-8 mb-6 hover:scale-105 transition duration-300 ease-in"
+        className="mt-6 mb-6 font-bickhamscript text-4xl hover:scale-105 transition duration-300 ease-in"
       >
-        <div className="font-bickhamscript text-4xl">
-          <p>1 rue de la Boule Rouge</p>
-          <p>Paris 09</p>
-        </div>
+        <p>1 rue de la Boule Rouge</p>
+        <p>Paris 09</p>
       </a>
       <a
         href="https://www.instagram.com/laboulerougeparis/"
